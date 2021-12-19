a3cc224c299144e2045b2f605ee4626cfb6e4733
Flavio Oliveira <flavio_r_o@yahoo.com.br>
2021-12-19 16:57:19 -0300

    Minor changes

--- resources/js/components/Header/Header.js
+++ resources/js/components/Header/Header.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import bootstrap from ''
 
+function Header() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
 
-export default class Header extends Component {
-    render() {
-        return (
-            <AppBar 
-                position="static"
-                style={{ color: "black", backgroundColor: "#F2AA4CFF" }}
-            >
-                
-                <Toolbar
-                    style={{ display: " flex", justifyContent: "space-between" }}
-                >
-                    <Typography variant="h6">
-                        Todo List Collegia
-                    </Typography>          
-                </Toolbar>
-            </AppBar>
-        );
-    }
+                        <div className="card-body">I'm an example component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
+
+if (document.getElementById('header')) {
+    ReactDOM.render(<Example />, document.getElementById('header'));
 }
