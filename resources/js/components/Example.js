0c41ebc713d8352588969d91beeaf6ea2f5cc5a9
Flavio Oliveira <flavio_r_o@yahoo.com.br>
2021-12-20 19:52:08 -0300

    Add methods

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,23 +1,115 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Axios from 'axios';
 
-function Example() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
+class Example extends Component {
 
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: 0,
+            title:'',
+            description: '',
+            todos: []
+        }
+    }
+    componentDidMount() {
+        this.getAll();
+    }
+    getAll() {
+        Axios.get(`http://localhost:8000/api/todoList`)
+            .then(res => {
+                this.setState({ todos: res.data,
+                id:0,
+                title:'',
+                description: '' });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+    getOne(todo) {
+        this.setState({
+            id: todo.id,
+            title: todo.title,
+            description: todo.description
+        });
+    }
+
+    delete(id){
+        Axios.delete(`http://localhost:8000/api/todo/${id}`)
+        .then((res) => {
+            this.getAll();
+        })
+    }
+
+    submit(event,id) {
+        event.preventDefault();
+        if(this.state.id == 0) {
+            Axios.post('http://localhost:8000/api/todo/store', {
+                title: this.state.title,
+                description: this.state.description
+            })
+                .then(res => {
+                    this.getAll();
+                })
+        } else {
+            Axios.put(`http://localhost:8000/api/todo/${id}`, {
+                title: this.state.title,
+                description: this.state.description
+            })
+                .then(res => {
+                    this.getAll();
+                })
+        }
+    }
+    titleChange(event) {
+        this.setState({
+            title: event.target.value
+        })
+    }
+    descriptionChange(event) {
+        this.setState({
+            description: event.target.value
+        })
+    }
+    render(){
+        return (
+            <div className="container">
+                <div className="row">
+                    <form onSubmit={(e) => this.submit(e, this.state.id)}>
+                        <div className="input-field col s4">
+                            <i className="material-icons prefix">input</i>
+                            <input onChange={(e) => this.titleChange(e)} value={this.state.title} />
+                        </div>
+                        <div className="input-field col s4">
+                            <i className="material-icons prefix">content_paste</i>
+                            <input onChange={(e) => this.descriptionChange(e)} value={this.state.description} />
+                        </div>
+                        <div className="col s4">
+                            <button type="submit" className="waves-effect waves-light btn">Save</button>
+                        </div>
+                    </form>
+
+
+
+
+
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>Title</td>
+                            <td>Description</td>
+                            <td>Edit</td>
+                            <td>Delete</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>"
             </div>
-        </div>
-    );
+        );
+    }
 }
-
 export default Example;
 
 if (document.getElementById('example')) {
